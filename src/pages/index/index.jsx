4647d2ef5d4051d4c30a6b7841b6da0a5ba6febb
Mick Coelho <mick.coelho@gmail.com>
2019-11-18 14:43:11 -0800

    Fix home

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 import Hero from '../../components/Title';
-import content from '../../../content/home.json';
+import content from '../../../content/home.md';
 import styles from './styles.css';
 
 export default class Home extends Component {
   render() {
-    const { hero } = content;
+    const { hero } = content.attributes;
 
     return (
       <section className={styles.container}>
